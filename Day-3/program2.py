effb384ddb3702dfca93265b0d83f3e6fd6d9ba9
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-05-20 23:30:31 +0530

    Update program2.py

--- Day-3/program2.py
+++ Day-3/program2.py
@@ -1,4 +1,4 @@
-problem statement : Write Python Program to Reverse a Given Number This is a Python Program to reverse a given number,
+problem statement : Write Python Program to Reverse a Given Number This is a Python Program to reverse a given number
 problem Description The program takes a number and reverses it and store it in another variable and show it
 
 solution: 
