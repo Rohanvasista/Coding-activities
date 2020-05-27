2e3d8f5a8666bc3c07f33d75a512b9bec478fc02
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-05-27 21:36:57 +0530

    Update program 2.py

--- Day-8/program 2.py
+++ Day-8/program 2.py
@@ -1,11 +1,14 @@
-  
+ problem statement :
+    
 #Python program to print all integers that aren't divisible either by 2 or 3 and lie between 1 and 50
 
 
-
+solution :
+  
 a = [i for i in range(1,51) if(i%2!=0 and i%3!=0)]
 print(a)
 
 
 Output:
+  
 [1, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31, 35, 37, 41, 43, 47, 49]
