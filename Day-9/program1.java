091d947420d1115f1144e7f14e9ddc41c487df4d
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-05-28 18:00:12 +0530

    Update program1.java

--- Day-9/program1.java
+++ Day-9/program1.java
@@ -1,3 +1,11 @@
+problem statement :
+
+Write a function that accepts a string consisting entiring of brackets ({}) and returns whether it is balanced. Every "opening" bracket must be followed by a closing bracket of the same type. There can also be nested brackets, which adhere to the same rule.
+f('()[]{}(([])){[()][]}') // true
+f('())[]{}') // false
+	
+	
+solution :
 
 import java.util.Stack;
 public class Main {
@@ -48,3 +56,12 @@ public class Main {
 		System.out.println(brackets("())[]{]{}")); // false
 	}
 }
+output :
+
+true
+false
+	
+	
+	
+	
+	
