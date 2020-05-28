78fb0ff05dd0d41a2d8a611c90aa44cd8ca6e637
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-05-28 18:06:57 +0530

    Add files via upload

---
+++ Day-9/program1.c
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std; 
+
+int find_value(int a[], int n, int k) 
+{ 
+    
+    int sum = 0; 
+  
+   
+    for (int i = 0; i < n; i++) { 
+        sum += a[i]; 
+    } 
+ 
+    return sum % k; 
+} 
+  
+
+int main() 
+{ 
+    int n = 5, k = 3; 
+    int a[] = { 12, 4, 13, 0, 5 }; 
+    cout << find_value(a, n, k); 
+    return 0; 
+}
