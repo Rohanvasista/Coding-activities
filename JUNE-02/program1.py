026a69acd4a5ddd4d34011a9e8a43936d714b939
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-06-02 21:17:27 +0530

    Update program1.py

--- JUNE-02/program1.py
+++ JUNE-02/program1.py
@@ -1,8 +1,13 @@
 problem statement :
+Python program to return a list containing first and last element using list slicing method
 
-
-
+solution:
 test_list = [2,4,5, 6, 7] 
 print ("The original list is : " +  str(test_list)) 
 res = [ test_list[0], test_list[-1] ] 
 print ("The first and last element of list are : " +  str(res))
+
+output:
+The original list is : [2, 4, 5, 6, 7]
+The first and last element of list are : [2,7]
+  
