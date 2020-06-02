8b0702d2f93bf14f9e7689385c8ae0f12838308f
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-06-02 21:10:45 +0530

    Delete program1.py

--- program1.py
+++
@@ -1,8 +0,0 @@
-problem statement :
-
-
-
-test_list = [2,4,5, 6, 7] 
-print ("The original list is : " +  str(test_list)) 
-res = [ test_list[0], test_list[-1] ] 
-print ("The first and last element of list are : " +  str(res))
