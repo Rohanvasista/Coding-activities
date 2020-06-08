65052ab54048327bf466f2aaa2e67a44760599ae
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-06-08 22:20:06 +0530

    Update p2.py

--- JUNE-08/p2.py
+++ JUNE-08/p2.py
@@ -1,12 +1,14 @@
-"""Write a python function that will take a string and checks whether it is a palindrome or not. Return If it a palindrome, print true else print false"""
+problrm statement:
+Write a python function that will take a string and checks whether it is a palindrome or not.Return If it a palindrome, print true else print false"""
 
-
+solution:
 s = str(input("Enter the string\n"))
 a = s[::-1]
 if a==s:
   print("True")
 else:
   print("False")
+
 
 Output:
 Enter the string
