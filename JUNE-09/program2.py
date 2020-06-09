89c5ffd6ccd1c427921f39753d45e78a88906c24
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-06-09 23:18:23 +0530

    Add files via upload

---
+++ JUNE-09/program2.py
@@ -0,0 +1,20 @@
+problem statement:
+Python Program to Reverse a String Using Recursion
+
+
+solution:
+def reverse(str):
+    if len(str) == 0:
+        return str
+    else:
+        return reverse(str[1:]) + str[0]
+
+mystr =str(input("enter the sting"))
+print("The Given String  is: ", mystr)
+print("Reversed String is: ", reverse(mystr))
+
+
+output:
+enter the stingpython
+The Given String  is:  python
+Reversed String is:  nohtyp
