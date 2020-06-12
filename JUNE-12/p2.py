6e5c764be4739b621d7785c885f053d048496622
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2020-06-12 21:40:12 +0530

    Add files via upload

---
+++ JUNE-12/p2.py
@@ -0,0 +1,18 @@
+problem statement:
+Python program to Count the Number of Vowels Present in a String using Sets
+
+
+
+solution:
+s = str(input("Enter the string : "))
+v = set("aeiouAEIOU")
+c =0
+for i in s:
+  if i in v:
+      c = c+1
+print("Number of vowels : ",c)
+
+
+Output:
+Enter the string : python is good
+Number of vowels :  4
